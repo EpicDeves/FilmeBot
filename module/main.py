7782adbcc7b74f9d79bc270403797ea95f0d8178
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-01 13:09:24 +0530

    Update main.py

--- module/main.py
+++ module/main.py
@@ -35,3 +35,21 @@ from config import *
 from utils import Media, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        
+#=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
