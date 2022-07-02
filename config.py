948b7a95527a8ec3024328e95a035e7849cd397b
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-02 08:45:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+
+# Bot settings
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+PRIVATE_LOG = environ['PRIVATE_LOG']
+# MongoDB information
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+OWNER_ID = environ['OWNER_ID']
+LOG_CHANNEL = environ['LOG_CHANNEL']
